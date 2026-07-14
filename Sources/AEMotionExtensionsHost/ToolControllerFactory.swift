c67fca1e185569906a95e0051661bd433745fa56
woo642778-art <woo642778@gmail.com>
2026-07-14 07:23:14 -0700

    refactor: restrict global tool descriptors to independent utilities

--- Sources/AEMotionExtensionsHost/ToolControllerFactory.swift
+++ Sources/AEMotionExtensionsHost/ToolControllerFactory.swift
@@ -27,7 +27,11 @@ enum ToolControllerFactory {
     }
 
     static func descriptor(for toolID: String) -> ToolDescriptor? {
-        ToolRegistry.all.first { $0.id == toolID }
+        let placement = ToolPlacementRegistry.placement(for: toolID)
+        guard placement == .extensionsHub || toolID == "preset.library" || toolID == "project.reliability" else {
+            return nil
+        }
+        return ToolRegistry.all.first { $0.id == toolID }
     }
 
     static func present(_ toolID: String, from presenter: UIViewController) {
