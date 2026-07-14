8689da3d23c7fe6928ec08815be04fa5c11c8619
woo642778-art <woo642778@gmail.com>
2026-07-14 06:01:12 -0700

    feat: place animation core in graph context

--- Sources/AEMotionExtensionsCore/ToolPlacement.swift
+++ Sources/AEMotionExtensionsCore/ToolPlacement.swift
@@ -6,8 +6,9 @@ public enum ToolPlacement: String, Codable, CaseIterable, Sendable {
 
 public enum ToolPlacementRegistry {
     private static let placements: [String: ToolPlacement] = [
-        "speed.remap": .timeline, "easing.curve": .graph, "cutout.person": .layer, "depth.map": .viewer,
-        "dead.frames": .clip, "camera.shake": .layer, "color.palette": .color, "preset.library": .project,
+        "speed.remap": .timeline, "easing.curve": .graph, "animation.core": .graph,
+        "cutout.person": .layer, "depth.map": .viewer, "dead.frames": .clip,
+        "camera.shake": .layer, "color.palette": .color, "preset.library": .project,
         "resource.hub": .extensionsHub, "host.diagnostics": .extensionsHub, "effects.integrity": .extensionsHub,
         "project.reliability": .project, "random.values": .extensionsHub, "expression.helper": .extensionsHub,
         "bpm.frames": .extensionsHub, "layer.offset": .extensionsHub,
