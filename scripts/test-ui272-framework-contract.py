2bfb112ad13b0041eeed85bdb546557529d523d9
woo642778-art <woo642778@gmail.com>
2026-07-27 03:05:08 -0700

    test: require Build 846 framework metadata

--- scripts/test-ui272-framework-contract.py
+++ scripts/test-ui272-framework-contract.py
@@ -16,7 +16,7 @@ checks = {
     "framework bundle": "AEMotionUI272.framework",
     "framework executable": 'CFBundleExecutable": "AEMotionUI272"',
     "release version": 'CFBundleShortVersionString": "2.7.2"',
-    "release build": 'CFBundleVersion": "845"',
+    "release build": 'CFBundleVersion": "846"',
     "install name": "@rpath/AEMotionUI272.framework/AEMotionUI272",
 }
 for name, token in checks.items():
