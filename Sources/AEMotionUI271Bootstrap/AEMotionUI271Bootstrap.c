9127be5daa4b535a81bd8bfd021543aff14451e3
woo642778-art <woo642778@gmail.com>
2026-07-26 20:39:06 -0700

    build: bootstrap the 2.7.2 UI framework

--- Sources/AEMotionUI271Bootstrap/AEMotionUI271Bootstrap.c
+++ Sources/AEMotionUI271Bootstrap/AEMotionUI271Bootstrap.c
@@ -1,9 +1,9 @@
 #include "AEMotionUI271Bootstrap.h"
 
-extern void AEMotionUI271Install(void) __attribute__((weak_import));
+extern void AEMotionUI272Install(void) __attribute__((weak_import));
 
-__attribute__((constructor)) static void AEMotionUI271BootstrapStart(void) {
-    if (AEMotionUI271Install) { AEMotionUI271Install(); }
+__attribute__((constructor)) static void AEMotionUI272BootstrapStart(void) {
+    if (AEMotionUI272Install) { AEMotionUI272Install(); }
 }
 
-void AEMotionUI271BootstrapLinkAnchor(void) {}
+void AEMotionUI272BootstrapLinkAnchor(void) {}
