54201160b1a8feb0e7972012da244e840b7cf95a
woo642778-art <woo642778@gmail.com>
2026-07-26 22:50:13 -0700

    chore: build AEMotionUI272 framework as build 842

--- scripts/build-ios-framework.sh
+++ scripts/build-ios-framework.sh
@@ -63,7 +63,7 @@ value = {
     "CFBundlePackageType": "FMWK",
     "CFBundleShortVersionString": "2.7.2",
     "CFBundleSupportedPlatforms": ["iPhoneOS"],
-    "CFBundleVersion": "841",
+    "CFBundleVersion": "842",
     "MinimumOSVersion": "15.0",
     "UIDeviceFamily": [1, 2],
     "UIRequiredDeviceCapabilities": ["arm64"],
