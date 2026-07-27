47be09fce620b3cefc472623e21e116fe2bfdbcb
woo642778-art <woo642778@gmail.com>
2026-07-26 18:46:02 -0700

    build: add AE Motion 2.7.1 legacy wrapper targets

--- Package.swift
+++ Package.swift
@@ -6,12 +6,31 @@ let package = Package(
     platforms: [.iOS(.v15), .macOS(.v13)],
     products: [
         .library(name: "AEMotionExtensionsCore", targets: ["AEMotionExtensionsCore"]),
-        .library(name: "AEMotionExtensionsHost", type: .dynamic, targets: ["AEMotionExtensionsHost", "AEMotionBootstrap"]),
+        .library(
+            name: "AEMotionExtensionsHost",
+            type: .dynamic,
+            targets: ["AEMotionUI271Host", "AEMotionUI271Bootstrap"]
+        ),
+        .library(
+            name: "AEMotionExtensionsLegacySource",
+            type: .dynamic,
+            targets: ["AEMotionExtensionsHost", "AEMotionBootstrap"]
+        ),
     ],
     targets: [
         .target(name: "AEMotionExtensionsCore"),
         .target(name: "AEMotionExtensionsHost", dependencies: ["AEMotionExtensionsCore"]),
-        .target(name: "AEMotionBootstrap", dependencies: ["AEMotionExtensionsHost"], publicHeadersPath: "include"),
+        .target(
+            name: "AEMotionBootstrap",
+            dependencies: ["AEMotionExtensionsHost"],
+            publicHeadersPath: "include"
+        ),
+        .target(name: "AEMotionUI271Host", dependencies: ["AEMotionExtensionsCore"]),
+        .target(
+            name: "AEMotionUI271Bootstrap",
+            dependencies: ["AEMotionUI271Host"],
+            publicHeadersPath: "include"
+        ),
         .testTarget(name: "AEMotionExtensionsCoreTests", dependencies: ["AEMotionExtensionsCore"]),
     ]
 )
