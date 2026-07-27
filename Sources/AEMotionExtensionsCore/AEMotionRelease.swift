d21d07b72ab63b03fcf70f98af8b19cf896e1adf
woo642778-art <woo642778@gmail.com>
2026-07-26 18:43:22 -0700

    feat: add AE Motion 2.7.1 release identity

---
+++ Sources/AEMotionExtensionsCore/AEMotionRelease.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+public enum AEMotionRelease {
+    public static let marketingVersion = "2.7.1"
+    public static let buildNumber = 839
+    public static let displayVersion = "\(marketingVersion) (\(buildNumber))"
+}
