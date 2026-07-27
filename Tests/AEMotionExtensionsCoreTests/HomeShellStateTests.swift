096b0244a9bedb55dad0081d48e17e5c5428d85b
woo642778-art <woo642778@gmail.com>
2026-07-26 18:43:55 -0700

    test: cover AE Motion 2.7.1 shell state

---
+++ Tests/AEMotionExtensionsCoreTests/HomeShellStateTests.swift
@@ -0,0 +1,39 @@
+import XCTest
+@testable import AEMotionExtensionsCore
+
+final class HomeShellStateTests: XCTestCase {
+    func testReleaseIdentity() {
+        XCTAssertEqual(AEMotionRelease.marketingVersion, "2.7.1")
+        XCTAssertEqual(AEMotionRelease.buildNumber, 839)
+    }
+
+    func testNonRootDestinationsHideNavigationAndReturnRestoresIt() {
+        for destination in [
+            HomeShellDestination.detail,
+            .editor,
+            .templatePreview,
+            .tool,
+        ] {
+            var state = HomeShellState()
+            HomeShellReducer.reduce(state: &state, event: .openDestination(destination))
+            XCTAssertFalse(state.isRootNavigationVisible)
+            HomeShellReducer.reduce(state: &state, event: .returnToRoot)
+            XCTAssertTrue(state.isRootNavigationVisible)
+        }
+    }
+
+    func testSelectingTabClosesCreateTrayWithoutChangingRootVisibility() {
+        var state = HomeShellState(selectedTab: .home, isCreateTrayExpanded: true)
+        HomeShellReducer.reduce(state: &state, event: .selectTab(.templates))
+        XCTAssertEqual(state.selectedTab, .templates)
+        XCTAssertFalse(state.isCreateTrayExpanded)
+        XCTAssertTrue(state.isRootNavigationVisible)
+    }
+
+    func testCreateTrayCannotOpenOutsideRootDestination() {
+        var state = HomeShellState(destination: .editor)
+        HomeShellReducer.reduce(state: &state, event: .setCreateTrayExpanded(true))
+        XCTAssertFalse(state.isCreateTrayExpanded)
+        XCTAssertFalse(state.isRootNavigationVisible)
+    }
+}
