215cad9e607efc8785cb870aa9fde3478722d960
woo642778-art <woo642778@gmail.com>
2026-07-27 03:01:54 -0700

    chore: bump AE Motion to Build 846

--- Sources/AEMotionExtensionsCore/AEMotionRelease.swift
+++ Sources/AEMotionExtensionsCore/AEMotionRelease.swift
@@ -2,6 +2,6 @@ import Foundation
 
 public enum AEMotionRelease {
     public static let marketingVersion = "2.7.2"
-    public static let buildNumber = 845
+    public static let buildNumber = 846
     public static let displayVersion = "\(marketingVersion) (\(buildNumber))"
 }
