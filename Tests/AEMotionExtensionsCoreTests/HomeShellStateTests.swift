e53cf3733208b55d4198403836394e9f38965549
woo642778-art <woo642778@gmail.com>
2026-07-27 03:04:25 -0700

    test: update release identity to Build 846

--- Tests/AEMotionExtensionsCoreTests/HomeShellStateTests.swift
+++ Tests/AEMotionExtensionsCoreTests/HomeShellStateTests.swift
@@ -4,7 +4,7 @@ import XCTest
 final class HomeShellStateTests: XCTestCase {
     func testReleaseIdentity() {
         XCTAssertEqual(AEMotionRelease.marketingVersion, "2.7.2")
-        XCTAssertEqual(AEMotionRelease.buildNumber, 845)
+        XCTAssertEqual(AEMotionRelease.buildNumber, 846)
     }
 
     func testNonRootDestinationsHideNavigationAndReturnRestoresIt() {
