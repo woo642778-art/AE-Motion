2883a4637cb59212885e5fbd69adc28d78ac906b
woo642778-art <woo642778@gmail.com>
2026-07-27 21:57:21 -0700

    test: expect build 852 release identity

--- Tests/AEMotionExtensionsCoreTests/HomeShellStateTests.swift
+++ Tests/AEMotionExtensionsCoreTests/HomeShellStateTests.swift
@@ -4,7 +4,7 @@ import XCTest
 final class HomeShellStateTests: XCTestCase {
     func testReleaseIdentity() {
         XCTAssertEqual(AEMotionRelease.marketingVersion, "2.7.2")
-        XCTAssertEqual(AEMotionRelease.buildNumber, 851)
+        XCTAssertEqual(AEMotionRelease.buildNumber, 852)
     }
 
     func testNonRootDestinationsHideNavigationAndReturnRestoresIt() {
